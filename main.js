d9896a66f020e50d0fc8b86a38a749c1b3b73b5e
Rebecca Nachison <rnachison@Rebeccas-MacBook-Air.local>
2014-10-09 12:30:01 -0700

    reconfigure board to use ng-repeat

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 // console.log("pastelgoth")
 var witchTic = angular.module("witchTic", []);
-
+var count = 0;
 witchTic.controller('Controlled', function ($scope) {
 
-	$scope.launchFamiliar = function () {
-		$scope.owlLaunch = "owl";
-	};
+    $scope.testString = "All connected, boo!" ;
+
+    $scope.theCells = [
+    {status: "A"}, 
+    {status: "B"}, 
+    {status: "C"}, 
+    {status: "D"}, 
+    {status: "E"}, 
+    {status: "F"}, 
+    {status: "G"}, 
+    {status: "H"}, 
+    {status: "I"}
+    ]  ;
+
+	// $scope.launchFamiliar = function () {
+ //        if ((count % 2) === 1) {
+ //            $scope.owlLaunch = true;
+ //        } else {
+ //            $scope.batLaunch = true;
+ //        };
+ //    };
+ //    $scope.launchTic = function () {
+ //        if ((count % 2) === 1) {
+ //            $scope.isowl = true;
+ //        } else {
+ //            $scope.isbat = true;
+ //        };
+ //    };
 });
 
