3efb1049e9f2179c4003cd98f3ad17e2fc3918f7
Rebecca Nachison <rnachison@Rebeccas-MacBook-Air.local>
2014-10-17 13:31:26 -0700

    try to stop cheating

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 // console.log("pastelgoth")
+var myHttp;
+
 var witchTic = angular.module("witchTic", ["firebase"]);
-witchTic.controller('Controlled', function ($scope, $firebase) {
+var FB;
+witchTic.controller('Controlled', function ($scope, $firebase, $http) {
 
+    myHttp = $http;
+
+    var witchRef = new Firebase("https://witch-game.firebaseio.com/")
     $scope.remoteGameContainer = 
-    $firebase(new Firebase("https://witch-game.firebaseio.com/databaseGameContainer")) ;
+    $firebase(witchRef) ;
+        FB=($scope.remoteGameContainer);
 
-    $scope.testString = "All connected, boo!" ;
+    // $scope.testString = "All connected, boo!" ;
 
-    $scope.theCells = [
-    {status: "X", num: 0}, 
-    {status: "X", num: 1}, 
-    {status: "X", num: 2}, 
-    {status: "X", num: 3}, 
-    {status: "X", num: 4}, 
-    {status: "X", num: 5}, 
-    {status: "X", num: 6}, 
-    {status: "X", num: 7}, 
-    {status: "X", num: 8}
-    ]  ;
+    $scope.theCells = new Object() ;
+        $scope.theCells["0"] = {status: "X", num: 0} ; 
+        $scope.theCells["1"] = {status: "X", num: 1} ; 
+        $scope.theCells["2"] = {status: "X", num: 2} ; 
+        $scope.theCells["3"] = {status: "X", num: 3} ; 
+        $scope.theCells["4"] = {status: "X", num: 4} ; 
+        $scope.theCells["5"] = {status: "X", num: 5} ; 
+        $scope.theCells["6"] = {status: "X", num: 6} ; 
+        $scope.theCells["7"] = {status: "X", num: 7} ; 
+        $scope.theCells["8"] = {status: "X", num: 8} ;
 
 // Set click counter at 0
 
@@ -29,12 +35,6 @@ witchTic.controller('Controlled', function ($scope, $firebase) {
     $scope.batVictor = 0 ;
 
 
-// Create arrays to store marked cells in
-
-    // $scope.owlCells = [];
-    // $scope.batCells = [];
-
-
 // Create win/tie variables
 
     $scope.owlBidden = false;
@@ -42,19 +42,26 @@ witchTic.controller('Controlled', function ($scope, $firebase) {
     $scope.tieGame = false;
     $scope.endOfGame = false;
 
-    $scope.gameContainer = {
-        theBoard: $scope.theCells,
-        clickCounter: $scope.counter,
-        owlWon: $scope.owlBidden,
-        batWon: $scope.batBidden,
-        aTie: $scope.tieGame,
-        owlWins: $scope.owlVictor,
-        batWins: $scope.batVictor,
-        endGame: $scope.endOfGame
-    } ;
+    witchTic.once("value", function (data) {
+
+        console.log(data.val()) ;
 
 
-    $scope.remoteGameContainer.$bind($scope, "gameContainer") ;
+        $scope.gameContainer = {
+            theBoard: $scope.theCells,
+            clickCounter: $scope.counter,
+            owlWon: $scope.owlBidden,
+            batWon: $scope.batBidden,
+            aTie: $scope.tieGame,
+            owlWins: $scope.owlVictor,
+            batWins: $scope.batVictor,
+            endGame: $scope.endOfGame
+        } ;
+
+
+        $scope.remoteGameContainer.$bind($scope, "gameContainer") ;
+
+    });
 
     $scope.$watch('gameContainer', function() {
     console.log('gameContainer changed!') ;
@@ -65,22 +72,16 @@ witchTic.controller('Controlled', function ($scope, $firebase) {
     $scope.reset = function () {
 
     // Reset cell statuses to clear markers
-        $scope.gameContainer.theBoard = [
-        {status: "X", num: 0}, 
-        {status: "X", num: 1}, 
-        {status: "X", num: 2}, 
-        {status: "X", num: 3}, 
-        {status: "X", num: 4}, 
-        {status: "X", num: 5}, 
-        {status: "X", num: 6}, 
-        {status: "X", num: 7}, 
-        {status: "X", num: 8}
-        ]  ;
+        $scope.gameContainer.theBoard["0"] = {status: "X", num: 0} ; 
+        $scope.gameContainer.theBoard["1"] = {status: "X", num: 1} ; 
+        $scope.gameContainer.theBoard["2"] = {status: "X", num: 2} ; 
+        $scope.gameContainer.theBoard["3"] = {status: "X", num: 3} ; 
+        $scope.gameContainer.theBoard["4"] = {status: "X", num: 4} ; 
+        $scope.gameContainer.theBoard["5"] = {status: "X", num: 5} ; 
+        $scope.gameContainer.theBoard["6"] = {status: "X", num: 6} ; 
+        $scope.gameContainer.theBoard["7"] = {status: "X", num: 7} ; 
+        $scope.gameContainer.theBoard["8"] = {status: "X", num: 8} ;
 
-    // Reset owl and bat arrays
-
-        // $scope.owlCells = [];
-        // $scope.batCells = [];
 
     // reset ng-show so no winner is displayed
 
@@ -109,24 +110,15 @@ witchTic.controller('Controlled', function ($scope, $firebase) {
         }
         console.log("Cell is now " + thisCell.status) ;
 
-// Create function to add cell values to appropriate arrays
-
-        // if (thisCell.status == "O") {
-        //     $scope.owlCells.push(thisCell.num) ;
-        //     console.log($scope.owlCells) ;
-        // } else {
-        //     $scope.batCells.push(thisCell.num)
-        //     console.log($scope.batCells) ;
-        // };
 
 
 // Define winner by checking for winning num combinations in arrays
 
-        if ((($scope.gameContainer.theBoard[0].status == "O") && ($scope.gameContainer.theBoard[1].status == "O") && ($scope.gameContainer.theBoard[2].status == "O")) || (($scope.gameContainer.theBoard[3].status == "O") && ($scope.gameContainer.theBoard[4].status == "O") && ($scope.gameContainer.theBoard[5].status == "O")) || (($scope.gameContainer.theBoard[6].status == "O") && ($scope.gameContainer.theBoard[7].status == "O") && ($scope.gameContainer.theBoard[8].status == "O")) || (($scope.gameContainer.theBoard[0].status == "O") && ($scope.gameContainer.theBoard[3].status == "O") && ($scope.gameContainer.theBoard[6].status == "O")) || (($scope.gameContainer.theBoard[1].status == "O") && ($scope.gameContainer.theBoard[4].status == "O") && ($scope.gameContainer.theBoard[7].status == "O")) || (($scope.gameContainer.theBoard[2].status == "O") && ($scope.gameContainer.theBoard[5].status == "O") && ($scope.gameContainer.theBoard[8].status == "O")) || (($scope.gameContainer.theBoard[0].status == "O") && ($scope.gameContainer.theBoard[4].status == "O") && ($scope.gameContainer.theBoard[8].status == "O")) || (($scope.gameContainer.theBoard[2].status == "O") && ($scope.gameContainer.theBoard[4].status == "O") && ($scope.gameContainer.theBoard[6].status == "O"))) {
+        if ((($scope.gameContainer.theBoard["0"].status == "O") && ($scope.gameContainer.theBoard["1"].status == "O") && ($scope.gameContainer.theBoard["2"].status == "O")) || (($scope.gameContainer.theBoard["3"].status == "O") && ($scope.gameContainer.theBoard["4"].status == "O") && ($scope.gameContainer.theBoard["5"].status == "O")) || (($scope.gameContainer.theBoard["6"].status == "O") && ($scope.gameContainer.theBoard["7"].status == "O") && ($scope.gameContainer.theBoard["8"].status == "O")) || (($scope.gameContainer.theBoard["0"].status == "O") && ($scope.gameContainer.theBoard["3"].status == "O") && ($scope.gameContainer.theBoard["6"].status == "O")) || (($scope.gameContainer.theBoard["1"].status == "O") && ($scope.gameContainer.theBoard["4"].status == "O") && ($scope.gameContainer.theBoard["7"].status == "O")) || (($scope.gameContainer.theBoard["2"].status == "O") && ($scope.gameContainer.theBoard["5"].status == "O") && ($scope.gameContainer.theBoard["8"].status == "O")) || (($scope.gameContainer.theBoard["0"].status == "O") && ($scope.gameContainer.theBoard["4"].status == "O") && ($scope.gameContainer.theBoard["8"].status == "O")) || (($scope.gameContainer.theBoard["2"].status == "O") && ($scope.gameContainer.theBoard["4"].status == "O") && ($scope.gameContainer.theBoard["6"].status == "O"))) {
             $scope.gameContainer.owlWon = true;
             $scope.gameContainer.endGame = true;
             $scope.gameContainer.owlWins = $scope.gameContainer.owlWins + 1 ;
-        } else if ((($scope.gameContainer.theBoard[0].status == "B") && ($scope.gameContainer.theBoard[1].status == "B") && ($scope.gameContainer.theBoard[2].status == "B")) || (($scope.gameContainer.theBoard[3].status == "B") && ($scope.gameContainer.theBoard[4].status == "B") && ($scope.gameContainer.theBoard[5].status == "B")) || (($scope.gameContainer.theBoard[6].status == "B") && ($scope.gameContainer.theBoard[7].status == "B") && ($scope.gameContainer.theBoard[8].status == "B")) || (($scope.gameContainer.theBoard[0].status == "B") && ($scope.gameContainer.theBoard[3].status == "B") && ($scope.gameContainer.theBoard[6].status == "B")) || (($scope.gameContainer.theBoard[1].status == "B") && ($scope.gameContainer.theBoard[4].status == "B") && ($scope.gameContainer.theBoard[7].status == "B")) || (($scope.gameContainer.theBoard[2].status == "B") && ($scope.gameContainer.theBoard[5].status == "B") && ($scope.gameContainer.theBoard[8].status == "B")) || (($scope.gameContainer.theBoard[0].status == "B") && ($scope.gameContainer.theBoard[4].status == "B") && ($scope.gameContainer.theBoard[8].status == "B")) || (($scope.gameContainer.theBoard[2].status == "B") && ($scope.gameContainer.theBoard[4].status == "B") && ($scope.gameContainer.theBoard[6].status == "B"))) {
+        } else if ((($scope.gameContainer.theBoard["0"].status == "B") && ($scope.gameContainer.theBoard["1"].status == "B") && ($scope.gameContainer.theBoard["2"].status == "B")) || (($scope.gameContainer.theBoard["3"].status == "B") && ($scope.gameContainer.theBoard["4"].status == "B") && ($scope.gameContainer.theBoard["5"].status == "B")) || (($scope.gameContainer.theBoard["6"].status == "B") && ($scope.gameContainer.theBoard["7"].status == "B") && ($scope.gameContainer.theBoard["8"].status == "B")) || (($scope.gameContainer.theBoard["0"].status == "B") && ($scope.gameContainer.theBoard["3"].status == "B") && ($scope.gameContainer.theBoard["6"].status == "B")) || (($scope.gameContainer.theBoard["1"].status == "B") && ($scope.gameContainer.theBoard["4"].status == "B") && ($scope.gameContainer.theBoard["7"].status == "B")) || (($scope.gameContainer.theBoard["2"].status == "B") && ($scope.gameContainer.theBoard["5"].status == "B") && ($scope.gameContainer.theBoard["8"].status == "B")) || (($scope.gameContainer.theBoard["0"].status == "B") && ($scope.gameContainer.theBoard["4"].status == "B") && ($scope.gameContainer.theBoard["8"].status == "B")) || (($scope.gameContainer.theBoard["2"].status == "B") && ($scope.gameContainer.theBoard["4"].status == "B") && ($scope.gameContainer.theBoard["6"].status == "B"))) {
             $scope.gameContainer.batWon = true ;
             $scope.gameContainer.endGame = true;
             $scope.gameContainer.batWins = $scope.gameContainer.batWins + 1 ;
